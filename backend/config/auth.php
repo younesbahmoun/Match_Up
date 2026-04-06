b1e76e65d32eef4b84771cea048fe50265c87733
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-04-06 23:51:21 +0100

    feat(auth): integrate JWT token generation and authentication guard

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -16,7 +16,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        // 'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -39,7 +40,11 @@ return [
 
     'guards' => [
         'web' => [
-            // 'driver' => 'session',
+            'driver' => 'session',
+            // 'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+        'api' => [
             'driver' => 'jwt',
             'provider' => 'users',
         ],
