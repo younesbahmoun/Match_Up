bad4fa457112362f6d7ed947ad656e1d1a1c2f97
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-04-06 23:49:59 +0100

    feat(auth): add user and auth API resources for response formatting

---
+++ backend/app/Http/Resources/Auth/AuthResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\Auth;
+
+use App\DataTransferObjects\Auth\AuthResultData;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'access_token' => $this->accessToken,
+            'token_type' => $this->tokenType,
+            'expires_in' => $this->expiresIn,
+            'user' => new UserResource($this->user),
+        ];
+    }
+}
