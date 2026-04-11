368e4aa8a157fe2559aebddda156b55efeece8bb
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-04-11 02:48:41 +0100

    refactor(resource): remove folder auth

--- backend/app/Http/Resources/AuthResource.php
+++ backend/app/Http/Resources/AuthResource.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Resources\Auth;
+namespace App\Http\Resources;
 
-use App\DataTransferObjects\Auth\AuthResultData;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+
 
 class AuthResource extends JsonResource
 {
