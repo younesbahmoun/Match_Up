99cb32880a73ec3dcc933e65078702b72790d107
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-04-11 02:09:09 +0100

    git commit -m "feat(owner): add  relations with terrain"

--- backend/app/Models/Owner.php
+++ backend/app/Models/Owner.php
@@ -12,4 +12,9 @@ class Owner extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function terrains()
+    {
+        return $this->hasMany(Terrain::class);
+    }
 }
